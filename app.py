fcd4dd0e2a319a28e6871d8a7d9077079b37985a
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-08-15 20:56:40 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -350,7 +350,8 @@ menu = st.sidebar.radio(
     )
 
 if menu == "📊 Fechamento & Financeiro":
-    IndentationError: unexpected indent
+
+IndentationError: unexpected indent
     st.title("📊 Painel Financeiro & Fechamento")
     
     df_vendas = pd.read_sql_query("SELECT * FROM vendas", conn)
