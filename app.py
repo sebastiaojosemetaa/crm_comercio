3a22603b6ac70d745e4e5c8101712d3cfd59237f
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-08-18 15:43:55 -0300

    Change 'Hello World' to 'Goodbye World'

--- app.py
+++ app.py
@@ -856,7 +856,7 @@ if st.form_submit_button("Salvar Cliente"):
                 st.dataframe(carregar_dados("SELECT * FROM produtos"), use_container_width=True)
 
                 with tab_forn:
-                st.subheader("Cadastrar Novo Fornecedor")
+                    st.subheader("Cadastrar Novo Fornecedor")
                 with st.form("form_cad_fornecedor"):
                     novo_forn = st.text_input("Nome do Fornecedor")
                     if st.form_submit_button("Salvar Fornecedor"):
