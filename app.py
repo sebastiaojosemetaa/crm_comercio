37afba97b86a0aacbd3d0c6b050e38d1daac9402
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-08-18 00:37:47 -0300

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -380,14 +380,14 @@ if st.button("🔄 Atualizar Valores com Estoque"):
     # O st.markdown e o st.dataframe devem estar alinhados com o 'if' acima
     st.markdown("---")
     st.dataframe(df_pedidos, use_container_width=True)
-                pdf_cli = gerar_pdf_tabela_pedidos(df_pedidos, cliente_nome=st.session_state.cliente_autenticado)
-                st.download_button(
-                    label=f"📄 Baixar Relatório de Pedidos ({st.session_state.cliente_autenticado}) em PDF",
-                    data=pdf_cli,
-                    file_name=f"Relatorio_Pedidos_{st.session_state.cliente_autenticado}.pdf",
-                    mime="application/pdf"
-                )
-            else:
+    pdf_cli = gerar_pdf_tabela_pedidos(df_pedidos, cliente_nome=st.session_state.cliente_autenticado)
+    st.download_button(
+    label=f"📄 Baixar Relatório de Pedidos ({st.session_state.cliente_autenticado}) em PDF",
+    data=pdf_cli,
+    file_name=f"Relatorio_Pedidos_{st.session_state.cliente_autenticado}.pdf",
+    mime="application/pdf"
+    )
+    else:
                 st.warning("Nenhum pedido encontrado para o seu usuário.")
 
 # ==========================================
