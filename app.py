029f9737060b9df96a289af75954c2d45fc44724
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-08-20 00:06:15 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -542,7 +542,7 @@ for col_f in ['fornecedor', 'Fornecedor']:
                         sugestao_fornec = str(linha_prod[col_f])
                         break
                 
-                for col_g in ['grupo', 'Grupo']:
+for col_g in ['grupo', 'Grupo']:
                     if col_g in cols_p and pd.notna(linha_prod[col_g]):
                         sugestao_grupo = str(linha_prod[col_g])
                         break
