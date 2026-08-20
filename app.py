21f3324f9a64a0d74e836cd1554e8c52f878b3a2
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-08-19 21:09:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -554,14 +554,14 @@ elif perfil_selecionado == "🔒 Administração / Vendedor":
         sugestao_grupo = str(linha_prod[col_g])
         break
 
-                with st.form("form_adicionar_item_pdv", clear_on_submit=False):
-                col_i1, col_i2, col_i3 = st.columns(3)
-            with col_i1:
-            qtd_item = st.number_input("Quantidade", min_value=0.1, step=1.0, value=1.0, key="pdv_qtd")
+with st.form("form_adicionar_item_pdv", clear_on_submit=False):
+    col_i1, col_i2, col_i3 = st.columns(3)
+with col_i1:
+    qtd_item = st.number_input("Quantidade", min_value=0.1, step=1.0, value=1.0, key="pdv_qtd")
                 
-            with col_i2:
-            idx_f = fornecedores_opt.index(sugestao_fornec) if sugestao_fornec in fornecedores_opt else 0
-            fornec_item = st.selectbox("Fornecedor", fornecedores_opt, index=idx_f, key="pdv_forn")
+with col_i2:
+    idx_f = fornecedores_opt.index(sugestao_fornec) if sugestao_fornec in fornecedores_opt else 0
+    fornec_item = st.selectbox("Fornecedor", fornecedores_opt, index=idx_f, key="pdv_forn")
 
                 v_unit_item = st.number_input("Preço Venda (R$)", min_value=0.0, step=1.0, value=float(sugestao_preco), key="pdv_v_unit")
 
