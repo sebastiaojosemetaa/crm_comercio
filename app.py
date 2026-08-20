0cdb3c14ec1e75d2cc5ee6d5f34b4b79a02c0fdb
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-08-20 19:35:43 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -581,7 +581,7 @@ elif perfil_selecionado == "🔒 Administração / Vendedor":
             st.session_state.setdefault("pdv_grupo", sugestao_grupo if sugestao_grupo in grupos_opt else grupos_opt[0])
 
             with col_s2:
-                st.info(f"Preço de Venda (Estoque): R$ {sugestao_preco:.2f}")
+                st.info(f"Preço Venda (Estoque de produtos): R$ {sugestao_preco:.2f}")
 
             with st.form("form_adicionar_item_pdv", clear_on_submit=False):
                 col_i1, col_i2, col_i3 = st.columns(3)
