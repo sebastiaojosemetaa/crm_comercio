078f378ba95f7be7907105ee9300c69960fc09d0
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-08-20 16:55:10 -0300

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -664,7 +664,7 @@ elif perfil_selecionado == "🔒 Administração / Vendedor":
                             st.rerun()
 
         # --- LÓGICA: ABERTURA E FECHAMENTO DE CAIXA ---
-        elif menu_admin == "🔓 Abertura e Fechamento de Caixa":
+if menu_admin == "🔓 Abertura e Fechamento de Caixa":
             st.title("🔓 Abertura e Fechamento de Caixa")
                     
             df_caixa_atual = carregar_dados("SELECT * FROM caixa_sessoes WHERE status = 'ABERTO'")
