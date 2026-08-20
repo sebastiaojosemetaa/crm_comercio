12051ba022bee3b690977269fd8dd67d44112f85
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-08-20 17:00:39 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -1137,7 +1137,7 @@ if menu_admin == "📥 Entrada de Estoque (Compras)":
             with aba_historico_compras:
                 st.dataframe(carregar_dados("SELECT * FROM compras"), use_container_width=True)
 
-        elif menu_admin == "📦 Estoque de Produtos":
+if menu_admin == "📦 Estoque de Produtos":
             st.title("📦 Estoque de Produtos e Preços")
             df_prods = carregar_dados("SELECT * FROM produtos")            
             if not df_prods.empty:
