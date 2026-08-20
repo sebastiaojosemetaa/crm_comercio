b3eac478f70bbfc87bde0a484ab102a8570b3a9a
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-08-20 00:04:46 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -537,7 +537,7 @@ elif perfil_selecionado == "🔒 Administração / Vendedor":
         except:
           pass
                 
-                for col_f in ['fornecedor', 'Fornecedor']:
+for col_f in ['fornecedor', 'Fornecedor']:
                     if col_f in cols_p and pd.notna(linha_prod[col_f]):
                         sugestao_fornec = str(linha_prod[col_f])
                         break
