4d3649246994a1e205da336d20d6f0903eddd81d
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-08-21 14:06:26 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def adequar_banco_e_migrar():
             cursor.execute("ALTER TABLE vendas ADD COLUMN data TEXT")
         except:
             pass
-cursor.execute("""
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS produtos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             nome TEXT UNIQUE,
@@ -82,7 +82,7 @@ cursor.execute("""
         )
     """)
 
-cursor.execute("""
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS clientes (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             nome TEXT UNIQUE,
@@ -93,21 +93,21 @@ cursor.execute("""
         )
     """)
 
-cursor.execute("""
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS fornecedores (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             fornecedor TEXT UNIQUE
         )
     """)
 
-cursor.execute("""
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS grupos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             grupo TEXT UNIQUE
         )
     """)
 
-cursor.execute("""
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS compras (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             produto TEXT,
@@ -120,7 +120,7 @@ cursor.execute("""
         )
     """)
 
-cursor.execute("""
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS caixa_sessoes (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             data_abertura TEXT,
@@ -131,7 +131,7 @@ cursor.execute("""
         )
     """)
 
-cursor.execute("""
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS caixa_movimentacoes (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             sessao_id INTEGER,
