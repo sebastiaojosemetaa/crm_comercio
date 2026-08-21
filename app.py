45434a36eed9787c24c0b4baf94406494dc30d3c
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-08-20 21:36:06 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -600,18 +600,18 @@ with st.form("form_finalizar_pagamento_pdv"):
         if not df_caixa_aberto.empty:
             sessao_id = int(df_caixa_aberto.iloc[0]['id'])
                             
-                            for item in st.session_state.carrinho_pdv:
-                                salvar_pedido_ou_venda(
-                                    cliente=cliente_pdv,
-                                    produto=item['produto'],
-                                    fornecedor=item['fornecedor'],
-                                    grupo=item['grupo'],
-                                    quantidade=item['quantidade'],
-                                    valor_venda=item['valor_venda'],
-                                    forma_pagamento=f_pag,
-                                    valor_recebido=v_rec,
-                                    tipo="VENDA"
-                                )
+for item in st.session_state.carrinho_pdv:
+    salvar_pedido_ou_venda(
+    cliente=cliente_pdv,
+    produto=item['produto'],
+    fornecedor=item['fornecedor'],
+    grupo=item['grupo'],
+    quantidade=item['quantidade'],
+    valor_venda=item['valor_venda'],
+    forma_pagamento=f_pag,
+    valor_recebido=v_rec,
+    tipo="VENDA"
+   )
                             
                             cursor = conn.cursor()
                             cursor.execute("INSERT INTO caixa_movimentacoes (sessao_id, tipo, valor, descricao, data) VALUES (?, ?, ?, ?, ?)",
