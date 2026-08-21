ada65091e2ae9e97ae74252009770f8e0bbfb478
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-08-21 17:19:43 -0300

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -1102,7 +1102,7 @@ elif menu_admin == "📥 Entrada de Estoque (Compras)":
             with aba_historico_compras:
                 st.dataframe(carregar_dados("SELECT * FROM compras"), use_container_width=True)
 
-        elif menu_admin == "📦 Estoque de Produtos":
+elif menu_admin == "📦 Estoque de Produtos":
             st.title("📦 Estoque de Produtos e Preços")
             df_prods = carregar_dados("SELECT * FROM produtos")            
             if not df_prods.empty:
