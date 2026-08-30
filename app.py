1418d6e0db5e24694a35d17a5f495111ef3f4392
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-08-29 22:40:05 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -1243,7 +1243,6 @@ elif perfil_selecionado == "🔒 Administração / Vendedor":
                     st.dataframe(df_compras_recente, use_container_width=True)
                     
                     st.markdown("#### Gerenciar Compra Selecionada")
-                    id_compra_selecionada = st.selectbox("Selecione o ID da compra:", df_compras_recente["id"].tolist(), key="select_id_compra_v2")
                     
                     # Filtra de forma segura pelo ID
                     df_filtrado = df_compras_recente[df_compras_recente["id"] == id_compra_selecionada]
