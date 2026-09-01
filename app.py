f84327beacb266ae5a117150e10ba53171e8dca9
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-09-01 15:41:09 -0300

    Update add function to accept parameters

--- app.py
+++ app.py
@@ -1312,8 +1312,6 @@ elif perfil_selecionado == "🔒 Administração / Vendedor":
                         st.info("Nenhum pedido encontrado.")
                 except Exception as e:
                     st.error(f"Erro: {e}")
-                except Exception as e:
-                    st.error(f"Erro: {e}")
         
                         st.markdown("---")
                         st.markdown("### 📚 Pedidos Concluídos / Histórico Geral")
