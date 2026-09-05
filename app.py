d23529bf9b9d9b0822b3817c69869fd4988338b9
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-09-05 19:46:40 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -1097,7 +1097,6 @@ elif perfil_selecionado == "🔒 Administração / Vendedor":
                                     id_item = ultimo[0]
                                     cur.execute("""
                                         UPDATE vendas 
-                                        SET status = 'ORÇAMENTO (Convertido)', tipo = 'VENDA' 
                                         WHERE id = ?
                                     """, (int(id_item),))
                                     con_local.commit()
