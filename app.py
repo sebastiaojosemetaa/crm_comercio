be889c193648496f769983875bc34304b418e7fa
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-09-08 17:53:50 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -686,7 +686,7 @@ elif perfil_selecionado == "🔒 Administração / Vendedor":
                 else:
                     st.info("Nenhum registro encontrado para os filtros selecionados.")
             
-        elif menu_admin in ["🛒 Pedidos / Orçamentos", "🛒 Registrar Venda"]:
+        elif menu_admin in ["Pedidos / Orçamentos", "Registrar Venda"]:
             is_modo_pedido = (menu_admin == "🛒 Pedidos / Orçamentos")
             st.title(f"{menu_admin}")
             
