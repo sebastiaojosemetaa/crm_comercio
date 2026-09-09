556d4191a88719414a53f1034358c0ea145f8514
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-09-09 12:00:22 -0300

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -1218,7 +1218,7 @@ elif perfil_selecionado == "🔒 Administração / Vendedor":
                 # ... (código dos filtros de cliente, data inicial e final)
 
                 s_d1, s_d2 = d_inicio.strftime("%Y-%m-%d"), d_fin.strftime("%Y-%m-%d")
-                tabela_alvo_historico = 'vendas'
+                tabela_alvo_historico = 'pedidos'
                 
                 query_filt = f"SELECT * FROM {tabela_alvo_historico}"
                 df_registros = carregar_dados(query_filt)
