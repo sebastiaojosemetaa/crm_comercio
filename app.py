c152d2dd74c355126937a05523148339488d302d
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-09-10 14:54:40 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -1079,9 +1079,8 @@ elif perfil_selecionado == "🔒 Administração / Vendedor":
                 try:
                     conn_direto = sqlite3.connect("vendas.db")
                     df_parcial = pd.read_sql(
-                        "SELECT id, cliente, produto, quantidade, valor_venda as"
-                        " valor_compra, valor_total, tipo, status FROM pedidos WHERE status IS NULL"
-                        " OR status != 'Finalizado'",
+                        "SELECT id, cliente, produto, quantidade, valor_venda, valor_total, tipo,"
+                        " status FROM pedidos WHERE status IS NULL OR status != 'Finalizado'",
                         conn_direto,
                     )
                 except Exception as e:
