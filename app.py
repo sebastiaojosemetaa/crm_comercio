b8ca7a692ef44b8fc2b7ee2c234cac96e89527fd
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-09-17 15:10:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1243,8 +1243,7 @@ elif perfil_selecionado == "🔒 Administração / Vendedor":
                         st.rerun()
                     except Exception as e:
                         st.error(f"Erro ao converter pedido: {e}")
-                else:
-                    st.success("🎉 Nenhum pedido pendente para recebimento no momento!")        
+                        
 
                 st.divider()
                 st.subheader("📚 Pedidos Anteriores / Histórico Geral")
