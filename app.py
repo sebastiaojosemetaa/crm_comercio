109fc2bac7cb9b153b1c5b25bac00a5e701923b2
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-09-17 15:15:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1241,9 +1241,6 @@ elif perfil_selecionado == "🔒 Administração / Vendedor":
                         st.cache_data.clear()
                         st.success("✅ Pedido baixado com sucesso!")
                         st.rerun()
-                        
-              else:
-                st.success("🎉 Nenhum pedido pendente para recebimento no momento!")      
 
                 st.divider()
                 st.subheader("📚 Pedidos Anteriores / Histórico Geral")
