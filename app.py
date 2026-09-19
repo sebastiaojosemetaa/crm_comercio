429fbdf18f61689ec43934f06331d1565b23fd4a
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-09-19 19:39:01 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -287,7 +287,8 @@ def salvar_cliente_completo(nome, telefone, doc, endereco, cidade):
 # --- FORMULÁRIO ALINHADO EM 2 COLUNAS ---
 st.subheader("👤 Gerenciamento de Clientes")
     col_cli1, col_cli2 = st.columns(2)
-    
+    with st.form("form_cadastrar_cliente", clear_on_submit=True):
+        
     with col_cli1:
         txt_nome_cli = st.text_input("Nome do Cliente / Razão Social", key="cli_nome_cad")
         txt_doc_cli = st.text_input("CPF / CNPJ", key="cli_doc_cad")
