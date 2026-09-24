f1466ccaa124eb1e20eb9cfd4164f9e107284dc9
sebastiaojosemetaa <sebastiaojosemetaa@gmail.com>
2026-09-24 18:40:42 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -1679,7 +1679,6 @@ elif perfil_selecionado == "🔒 Administração / Vendedor":
         
             cols_esperadas = ['id', 'produto', 'quantidade', 'valor_compra', 'valor_venda', 'grupo', 'fornecedor']
             for c in cols_esperadas:
-                ...
     
             cols_esperadas = ['id', 'produto', 'quantidade', 'valor_compra', 'valor_venda', 'grupo', 'fornecedor']
             for c in cols_esperadas:
